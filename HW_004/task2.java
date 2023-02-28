1dc0b9128b23a4ad9cd56aff10e7ab627421a471
Alexey <lozhkin023@gmail.com>
2023-02-28 20:50:13 +0300

    add description task2

--- HW_004/task2.java
+++ HW_004/task2.java
@@ -1,5 +1,10 @@
 package HW_004;
-
+/*
+ * Реализуйте очередь с помощью LinkedList со следующими методами: 
+ * enqueue() - помещает элемент в конец очереди, 
+ * dequeue() - возвращает первый элемент из очереди и удаляет его,
+ * first() - возвращает первый элемент из очереди, не удаляя.
+ */
 public class task2 {
     public static void main(String[] args) {
         myQueue queue = new myQueue();

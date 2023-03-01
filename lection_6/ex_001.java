1deb5d1e7514f668fa876a7625ab43ea7381e6dd
Alexey <lozhkin023@gmail.com>
2023-03-02 01:57:04 +0300

    add lection6

---
+++ lection_6/ex_001.java
@@ -0,0 +1,12 @@
+
+public class ex_001 {
+    public static void main(String[] args) {
+        Worker w1 = new Worker();
+        w1.firstName = "Alexey";
+        w1.lastName = "Lozhkin";
+        w1.id = 4322299;
+        w1.salary = 500_000;
+        System.out.println(w1);
+    }
+
+}
